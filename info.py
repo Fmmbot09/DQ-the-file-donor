83880c4bdd7ae125e16d2e3c5ff3c7b1ea19f50b
Fmmbot09 <j32025026@gmail.com>
2024-10-09 07:20:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+BOT_TOKEN = environ['7923304115:AAFXbGC0JeHZzzixhJiHNKHYvKTxarvd8Uk']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
